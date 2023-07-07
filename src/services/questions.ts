4deacecce53ab355b114fdf582351723269e8f07
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-07-07 14:32:46 -0500

    fix number of questions

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -2,7 +2,7 @@ export async function getQuestions() {
   const response = await fetch('http://localhost:5173/data.json')
   const json = await response.json()
   const randomQuestions = json.sort(() => Math.random() - 0.5)
-  const totalQuestions = randomQuestions.slice(0, 5)
+  const totalQuestions = randomQuestions.slice(0, 10)
 
   return totalQuestions
 }
