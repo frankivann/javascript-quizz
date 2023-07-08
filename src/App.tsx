2d4f96a4e4c7849e4046547453775d6a8d12c756
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-07-08 14:32:23 -0500

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { QuizGame } from './components/QuizGame'
 import { Header } from './components/Header'
 import { QuizTitle } from './components/QuizTitle'
+import { QuizGame } from './components/QuizGame'
+import { Footer } from './components/Footer'
 import './App.css'
 
 export default function App() {
   return (
-    <div className='App'>
-      <Header />
-      <QuizTitle />
-      <QuizGame />
-    </div>
+    <>
+      <div className='App'>
+        <Header />
+        <QuizTitle />
+        <QuizGame />
+      </div>
+      <Footer />
+    </>
   )
 }
