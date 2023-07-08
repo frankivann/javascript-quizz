7060b1ad72790a0b72eab37cbe55498010587d69
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-07-08 10:04:00 -0500

    fix dynamic url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,5 +1,9 @@
+const API_URL = import.meta.env.PRO
+  ? 'https://frankivann-javascript-quizz.surge.sh'
+  : 'http://localhost:5173'
+
 export async function getQuestions() {
-  const response = await fetch('http://localhost:5173/data.json')
+  const response = await fetch(`${API_URL}/data.json`)
   const json = await response.json()
   const randomQuestions = json.sort(() => Math.random() - 0.5)
   const totalQuestions = randomQuestions.slice(0, 10)
