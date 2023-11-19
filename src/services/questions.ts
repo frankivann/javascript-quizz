3a2c66d218bf5020efef55742a5112f18c46ce0c
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-11-19 10:33:12 -0500

    remove dynamic url

--- src/services/questions.ts
+++ src/services/questions.ts
@@ -1,9 +1,5 @@
-const API_URL = import.meta.env.PROD
-  ? window.location.href
-  : 'http://localhost:5173'
-
 export async function getQuestions() {
-  const response = await fetch(`${API_URL}/data.json`)
+  const response = await fetch('/data.json')
   const json = await response.json()
   const randomQuestions = json.sort(() => Math.random() - 0.5)
   const totalQuestions = randomQuestions.slice(0, 10)
