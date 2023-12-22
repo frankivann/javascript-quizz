a2b2a236247971c358ba74186a1d9988e1ae73c3
Franklin Alvarado <franklinalvarado0921@gmail.com>
2023-12-22 09:58:08 -0500

    refactor app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
 import { Header } from './components/Header'
 import { QuizTitle } from './components/QuizTitle'
 import { QuizGame } from './components/QuizGame'
-import { Footer } from './components/Footer'
 import './App.css'
 
 export default function App() {
   return (
     <div className='App'>
-      <section className='Container'>
-        <Header />
-        <QuizTitle />
-        <QuizGame />
-      </section>
-      <Footer />
+      <Header />
+      <QuizTitle />
+      <QuizGame />
     </div>
   )
 }
